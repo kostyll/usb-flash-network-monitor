4190cbd90aafd3e7c5469ab1604ff92f62c55b48
Andrew <andrew@vasyltsiv.esx.ua>
2014-10-20 17:03:01 +0300

    some more refactoring

--- server/control_server.py
+++ server/control_server.py
@@ -4,7 +4,7 @@ from bottle import run, request, Bottle, static_file, response
 
 import simplejson
 
-import peewee
+from models import *
 
 from indexpage import IndexPage
 
@@ -21,70 +21,8 @@ from indexpage import IndexPage
 #     else:
 #         print(range)
 
-database_file = os.path.join(os.environ['HOME'],os.environ['USB_MONITOR_FILE'])
-
 class WrongIPAddressStrRepresentation(Exception):
     pass
-
-
-class IPField(peewee.Field):
-    db_field = 'int'
-    octet_offsets = [24,16,8,0]
-    def check_str_representation(self,value):
-        octets = str(value).split('.')
-        if len(octets) != 4:
-            raise WrongIPAddressStrRepresentation(value)
-        for octet in octets:
-            try:
-                octet = int(octet)
-            except ValueError:
-                raise WrongIPAddressStrRepresentation(value)
-            if octet<0 or octet>255:
-                raise WrongIPAddressStrRepresentation(value)
-
-    def db_value(self,value):
-        self.check_str_representation(value)
-        octets = map(int, str(value).split('.'))
-        octets = map(lambda x,y: x<<y, octets,self.octet_offsets)
-        result = sum(octets)
-        # print ('result = {}'.format(result))
-        return  result
-
-    def python_value(self,value):
-        octets = map(lambda y: (value >> y), self.octet_offsets)
-        # print ("octets {}".format(octets))
-        octets = map(lambda x: x % (2**8), octets)
-        # print ("octets {}".format(octets))
-        result = ".".join(map(str,octets))
-        # print ('result = {}'.format(result))
-        return result
-
-
-peewee.SqliteDatabase.register_fields({'int':'int'})
-
-db = peewee.SqliteDatabase(database_file)
-
-class BaseModel(peewee.Model):
-    class Meta:
-        database = db
-
-
-class Client(BaseModel):
-
-    ip_addr = IPField(unique=True)
-
-    description = peewee.CharField(max_length=200)
-
-
-class GeneralSerial(BaseModel):
-
-    number = peewee.CharField(unique=True)
-
-
-class ClientSerial(GeneralSerial):
-
-    client = peewee.ForeignKeyField(Client)
-    number = peewee.CharField(max_length=200)
 
 
 class UnregisteredMassStorageObserver(object):
@@ -218,8 +156,6 @@ def register_serial_at_machine(machine=None):
   return ok("")
 
 def main():
-    db.connect()
-    map(lambda x: x.create_table(fail_silently=True),[Client,GeneralSerial,ClientSerial])
     run(request_handler,port=8090,reloader=True)
 
 if __name__ == "__main__":
---
+++ server/models.py
@@ -0,0 +1,82 @@
+import os
+import peewee
+
+database_file = os.path.join(os.environ['HOME'],os.environ['USB_MONITOR_FILE'])
+
+maintained_tables = []
+
+__all__ = []
+
+def maintained(class_):
+    maintained_tables.append(class_)
+    class_name = (str(class_).split("'")[1].rpartition('.')[2])
+    # print class_name
+    __all__.append(class_name)
+    return class_
+
+
+class IPField(peewee.Field):
+    db_field = 'int'
+    octet_offsets = [24,16,8,0]
+    def check_str_representation(self,value):
+        octets = str(value).split('.')
+        if len(octets) != 4:
+            raise WrongIPAddressStrRepresentation(value)
+        for octet in octets:
+            try:
+                octet = int(octet)
+            except ValueError:
+                raise WrongIPAddressStrRepresentation(value)
+            if octet<0 or octet>255:
+                raise WrongIPAddressStrRepresentation(value)
+
+    def db_value(self,value):
+        self.check_str_representation(value)
+        octets = map(int, str(value).split('.'))
+        octets = map(lambda x,y: x<<y, octets,self.octet_offsets)
+        result = sum(octets)
+        # print ('result = {}'.format(result))
+        return  result
+
+    def python_value(self,value):
+        octets = map(lambda y: (value >> y), self.octet_offsets)
+        # print ("octets {}".format(octets))
+        octets = map(lambda x: x % (2**8), octets)
+        # print ("octets {}".format(octets))
+        result = ".".join(map(str,octets))
+        # print ('result = {}'.format(result))
+        return result
+
+
+peewee.SqliteDatabase.register_fields({'int':'int'})
+
+db = peewee.SqliteDatabase(database_file)
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+
+@maintained
+class Client(BaseModel):
+
+    ip_addr = IPField(unique=True)
+
+    description = peewee.CharField(max_length=200)
+
+
+@maintained
+class GeneralSerial(BaseModel):
+
+    number = peewee.CharField(unique=True)
+
+
+@maintained
+class ClientSerial(GeneralSerial):
+
+    client = peewee.ForeignKeyField(Client)
+    number = peewee.CharField(max_length=200)
+
+
+db.connect()
+map(lambda x: x.create_table(fail_silently=True),maintained_tables)
